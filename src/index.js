eb0c973d052496c8ecf61e3e166470e8af5ca98e
Valeryia Zhdanava <vzhd@ciklum.com>
2018-02-21 16:09:48 +0300

    added solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
 
-  add(element) {
-    // your implementation
-  }
+    constructor() {
+        this.array = [];
+        this.compareFunction = null;
+    }
 
-  at(index) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element);
+        return this.array;
+    }
 
-  get length() {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index];
+    }
 
-  toArray() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    toArray() {
+        return this.array;
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort(this.compareNumeric);
+        if (indices.length !== 1 && this.compareFunction === null) {
+            for (let i = 0; i < this.array.length; i++) {
+                for (let j = 0; j < indices.length; j++) {
+                    if (this.array[indices[j]] > this.array[indices[j + 1]]) {
+                        const temp = this.array[indices[j]];
+                        this.array[indices[j]] = this.array[indices[j + 1]];
+                        this.array[indices[j + 1]] = temp;
+                    }
+                }
+            }
+        } else if (this.compareFunction !== null) {
+            let list = [];
+            for (let i = 0; i < indices.length; i++) {
+                list.push(this.array[indices[i]]);
+            }
+            list.sort(this.compareFunction);
+            for (let j = 0; j < indices.length; j++) {
+                this.array[indices[j]] = list[j];
+            }
+        }
+        return this.array;
+    }
+
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
+
+    compareNumeric(a, b) {
+        return (a > b) ? 1 : -1;
+    }
 }
 
 module.exports = Sorter;
